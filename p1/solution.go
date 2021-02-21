a278e6458a40aeb221dbc9cd4704b04234cd2217
Vikas Kaushik <10323307+vkaushik@users.noreply.github.com>
2021-02-21 15:25:05 +0530

    Add solution description comments

--- p1/solution.go
+++ p1/solution.go
@@ -4,8 +4,13 @@ package p1
 // Given a string S, find length of the longest substring with all distinct characters.
 // For example, for input "abca", the output is 3 as "abc" is the longest substring with all distinct characters.
 
+// Solution:
 // LenOfLongestSubStWithUniqueChars returns length of the longest substring with all distinct characters.
 // TimeComplexity: O(n), SpaceComplexity: O(n), where n is the length of input string
+// Take two indexes named lefthHand and rightHand. The invariant property is that: "The characters between leftHand and rightHand are always unique." 
+// Both leftHand and rightHand keep moving from left to right.
+// The moment righHand finds a char that already exists between leftHand and rightHand, discard all the chars till the position just next of the duplicate char,
+// i.e. move left hand just after the previous location where the duplicate char (of the char at rightHand).
 func LenOfLongestSubStWithUniqueChars(str string) int {
 	l := len(str)
 	if l <= 1 {
